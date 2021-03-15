c5830afe6df8dc0a4f5f4566f5299be3759f6c03
Samuel Atkins <s.m.atkins73@gmail.com>
2021-03-15 15:19:56 -0400

    15/03/2021-Atkins-15:19:56 Completed preprocessing steps. Will now work on benchmark neural network.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,10 +1,46 @@
 from src.constants import Files, Directories
 from os import path
+from imblearn.over_sampling import SMOTE
+from sklearn import preprocessing
+
 import pandas as pd
 import zipfile
 import os
 
+SEED = 88
+
+
+def rebalance(df):
+    """
+    Re-balances a DataFrame by oversampling using SMOTE
+    :param df: Pandas DataFrame
+    :return: Pandas DataFrame
+    """
+    sm = SMOTE()
+    X, y = sm.fit_resample(df.drop('Class', axis=1), df['Class'])
+    return pd.concat([pd.DataFrame(X), pd.DataFrame(y)], axis=1)
+
+
+def standardize(df):
+    """
+    Standardizes the non-predictor columns in the input DataFrame.
+    :param df: Pandas DataFrame
+    :return: Pandas DataFrame
+    """
+    df_inputs = df.drop("Class", axis=1)
+    df_inputs_columns = df_inputs.columns
+    df_outputs = df["Class"]
+    standardizer = preprocessing.StandardScaler().fit(df_inputs)
+    df_inputs = standardizer.transform(df_inputs)
+    return pd.concat([pd.DataFrame(df_inputs, columns=df_inputs_columns), df_outputs], axis=1)
+
+
 def preprocess():
+    """
+    This function unzips and extracts data into a DataFrame. Then, this function re-balances and standardizes the raw
+    data before storing the result in the "data" folder.
+    :return: None
+    """
     if not path.exists(Files.raw_data):
         with zipfile.ZipFile(Files.zipped_data, 'r') as zip_ref:
             zip_ref.extractall(Directories.raw)
@@ -14,7 +50,9 @@ def preprocess():
 
     if not path.exists(Files.balanced_data) or True:
         df = pd.read_csv(Files.raw_data)
-        df = pd.DataFrame()
 
+        df = rebalance(df)
+        df = standardize(df)
 
         df.to_pickle(Files.balanced_data)
+
